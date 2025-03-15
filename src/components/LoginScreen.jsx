8390cb3ba3c25beb618379d3b098c88989936392
AlexReyes03 <20233TN160@utez.edu.mx>
2025-03-14 21:19:59 -0600

    [ADD] Añadiendo componente LoginScreen}

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    const storedUser = JSON.parse(localStorage.getItem("userData"));
+    if (storedUser && storedUser.pass === password) {
+      navigate("/data");
+    } else {
+      setError("Usuario o contraseña incorrectos");
+    }
+  }
+
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={handleLogin}>
+        <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Ingresar</button>
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+}
+
+export default LoginScreen;
